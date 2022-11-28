e3703c4b2ac1b9c31b253e93621537e47e51aeed
Dezzes <roderoven@mail.ru>
2022-11-28 12:54:52 +0300

    ci cd check

---
+++ components/HeroComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="hero">
+    <img src="../assets/images/movieHero.jpg" alt="movieHero" />
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss"></style>
